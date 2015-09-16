0822fe4bfd42b917494715caa7893854db4d1aaf
Theodor Tolstoy <theodor@tolstoy.se>
2015-09-16 12:19:53 +0200

    Återgick till Jetty, mer jobb med att flytta front-end-filer till bower

--- src/main/groovy/Routes.groovy
+++ src/main/groovy/Routes.groovy
@@ -4,15 +4,18 @@ import spark.template.mustache.MustacheTemplateEngine
 import spark.ModelAndView
 import spark.Request
 import spark.Response
-import spark.Route
 import static spark.Spark.*
-
 
 /**
  * Created by Theodor on 2015-09-09.
  */
 public class Routes implements SparkApplication {
     private final static MustacheTemplateEngine templateEngine = new MustacheTemplateEngine();
+
+    public static void main(String[] args){
+        SparkApplication app = new Routes();
+        app.init();
+    }
 
     @Override
     void init() {
@@ -26,8 +29,6 @@ public class Routes implements SparkApplication {
 
     private static ModelAndView index(final Request request, final Response response) {
         final Map map = new HashMap();
-        map.put("message", "hello");
-        map.put("name", "Theodor");
         map.put("pageTitle", "Uttag av data");
         new ModelAndView(map, "index.mustache");
     }
@@ -35,11 +36,9 @@ public class Routes implements SparkApplication {
     private static ModelAndView preview(final Request request, final Response response){
         final Map map = new HashMap();
         final Map queryParms = request.queryParams().collectEntries {
-            it->it.endsWith("[]") ? [it.replace("[]",""), request.queryParams(it)] : [it, request.queryParams(it)]
+            it->[it, request.queryParams(it)]
         };
         map.put("queryParms", queryParms)
-        map.put("message", "hello");
-        map.put("name", "Theodor");
         map.put("pageTitle", "Ladda ned fil");
         new ModelAndView(map, "preview.mustache");
     }
