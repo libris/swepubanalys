8c21c88064e4d23f6a06b7ea965ab067a8f88922
unknown <jimber@2014-002.kb.local>
2015-09-28 09:51:23 +0200

    Added webpack.config.js and basic client code structure

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: {
+        form: './src/main/resources/client/form.js',
+    },
+    output: {
+        filename: './src/main/resources/public/2.0/[name].bundle.js',
+    }
+};
