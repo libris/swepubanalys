a189b3b136dfc68e14fb1654ba12aa1f5fea0721
Jimmie <jimmie.berg@kb.se>
2015-09-30 15:17:44 +0200

    Added additional help-texts

--- src/main/resources/client/components/PublTypeInput/PublTypeInput.js
+++ src/main/resources/client/components/PublTypeInput/PublTypeInput.js
@@ -2,6 +2,7 @@
 
 // Components
 var AutocompleteInput = require('components/AutocompleteInput/AutocompleteInput.js');
+var HelpMixin = require('mixins/HelpMixin.js');
 
 /**
  * Publication Type Input-component
@@ -9,11 +10,18 @@ var AutocompleteInput = require('components/AutocompleteInput/AutocompleteInput.
  * @prop {Array} publTypeSuggestions
  */
 var PublTypeInput = {
+	mixins: [HelpMixin],
 	props: ['publTypes', 'publTypeSuggestions'],
 	template: require('./PubltypeInput.html'),
 	components: {
 		'autocomplete-input': AutocompleteInput,
-	}	
+	},
+	ready: function() {
+		this.initHelp({
+			title: 'PUBLIKATIONSTYPER',
+			content: require('./PublTypeInput.Help.html'),
+		});
+	}
 };
 
 module.exports = PublTypeInput;
