f46654228ba6be8d76a91ad2c7229a13935bb26e
Jimmie <jimmie.berg@kb.se>
2015-10-07 09:32:28 +0200

    HelpMixin popovers may be triggered by any given element. Form-helps are triggered by buttons instead

---
+++ src/main/resources/client/components/HelpInitiatorButton/HelpInitiatorButton.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// CSS
+require('./HelpInitiatorButton.css');
+
+/**
+ * A button which can be used to trigger a help-dialog or popover
+ */
+var HelpInitiatorButton = {
+	template: require('./HelpInitiatorButton.html')
+};
+
+module.exports = HelpInitiatorButton;
--- src/main/resources/client/mixins/HelpMixin.js
+++ src/main/resources/client/mixins/HelpMixin.js
@@ -4,24 +4,45 @@
 var $ = require('jquery');
 window.jQuery = $;
 require('bootstrap/dist/js/bootstrap.min.js');
+// Components
+var HelpInitiatorButton = require('components/HelpInitiatorButton/HelpInitiatorButton.js');
 
 /**
- * Mixin for creating Help-popovers. Just call initHelp({ ... }) from 
- * the ready() hook of any component
+ * Mixin used to append a Help-popover to an element. Just call initHelp({ ... }) from ready-hook.
+ * 
+ * Which element should the popover anchor to? Provide one explicitly, otherwise this mixin chooses the component root-node
+ * 
+ * Which element should trigger the popover to show? Provide one explicitly, otherwise the mixin chooses an element using
+ * the className 'helpInitiator' or component root-node
  */
 var HelpMixin = {
+	components: {
+		'help-initiator-button': HelpInitiatorButton
+	},
 	methods: {
 		/**
-		 * Initialise Help popover
+		 * Initiates the help-popover
 		 * @param {Object} conf
 		 */
 		initHelp: function(conf) {
-			var el = $(this.$el);
-			conf = conf || {};
+			var el = this.$el;
+			conf.initiatorElement = conf.initiatorElement || (el.getElementsByClassName('helpInitiator')[0] || el);
+			conf.anchorToElement = conf.anchorToElement || el;
+			this.createPopover(conf);
+		},
+		/**
+		 * Creates a popover given an initiatorElement and anchorToElement
+		 * @param {Object} conf
+		 */
+		createPopover: function(conf) {
+			if(!conf.initiatorElement || !conf.anchorToElement) {
+				console.error('*** HelpMixin.js: initiatorElement and anchorToElement args not provided');
+				return false;
+			}
 			var marginLeft = conf.marginLeft || '15px';
 			var marginTop = conf.marginTop || '15px';
 			// Create popover
-			$(el).popover({
+			$(conf.anchorToElement).popover({
 				trigger: 'manual',
 				placement: 'right',
 				template: '<div class="popover" style="margin-left: ' + marginLeft + '; margin-top: ' + marginTop + '"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>',
@@ -30,16 +51,28 @@ var HelpMixin = {
 				html: true,
 				container: 'body',
 			});
-			// If user clicks...
+			/**
+			 * Handle show/hide manually
+			 */
 			$(document).on('click', function(e) {
-				// ...on an element which is not contained within this.$el...
-				if(!($.contains(this.$el, e.target))) {
-					$(el).popover('hide');
-					$(el).data('popoverVisible', false);
+				var initiatorElement = conf.initiatorElement;
+				var anchorToElement = conf.anchorToElement;
+				// If click on, or inside of, initiatorElement
+				if(initiatorElement === e.target || $.contains(initiatorElement, e.target)) {
+					// Show popover if not visible
+					if(!$(anchorToElement).data('popoverVisible')) {
+						$(anchorToElement).popover('show');
+						$(anchorToElement).data('popoverVisible', true);
+					}
 				}
-				else if(!$(el).data('popoverVisible')) {
-					$(el).popover('toggle');
-					$(el).data('popoverVisible', true);
+				// If click on, or inside of, anchorToElement
+				else if((anchorToElement === e.target || $.contains(anchorToElement, e.target)) && $(anchorToElement).data('popoverVisible') === true) {
+					// Do nothing
+				}
+				// Else hide popover
+				else {
+					$(anchorToElement).popover('hide');
+					$(anchorToElement).data('popoverVisible', false);
 				}
 			}.bind(this));
 		}
