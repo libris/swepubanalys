9de16c4e5d4b572dc553eaf6fcf3a2b370d157bd
Jimmie <jimmie.berg@kb.se>
2015-10-13 08:38:06 +0200

    FieldValidationMixin can now use async functions using Q-promises

--- src/main/resources/client/mixins/FieldValidationMixin.js
+++ src/main/resources/client/mixins/FieldValidationMixin.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// Vendor
+var Q = require('q');
 // Components
 var FormErrorLabel = require('components/FormErrorLabel/FormErrorLabel.js');
 
 /*
- * Field Validation mixin. Is used to test props.field.value against props.field.test.expression.
+ * Field Validation Mixin. Used to test props.field.value against props.field.test functions, or custom
+ * test functions.
  * @prop {Object} field
  * @prop {Object} test
  */
@@ -19,40 +22,55 @@ var FieldValidationMixin = {
 	},
 	methods: {
 		/**
-		 * The function executes all provided validation functions and pushes possible errors to data.errors
+		 * The function executes all provided validation functions and pushes possible errors to data.errors.
+		 * Provided functions must use a callback, not return, to support async checks.
 		 * @param {Array} funcs
 		 */
-		setValidationListeners: function(funcs) {
+		setValidationListeners: function(funcs) {		
+			/**
+			 * This function takes a (possibly async) test function and creates a promise. 
+			 * If test failed, it adds corresponding error to data.errors
+			 * @param {Function} func
+			 */
+			var testFuncFactory = function(func, errors) {
+				var deferred = Q.defer();
+				func(function(passed) {
+					if(passed !== true) { 
+						errors.push(passed); // Add error message
+					}
+					deferred.resolve();
+				});
+				return deferred.promise;
+			}
 			var t; // Timeout reference
-			var dt = 1000;
+			var dt = 800;
+			// On field change
 			this.$watch('field', function(val) {
 				this.$set('errors', []); // Reset error messages
 				clearTimeout(t); // Clear old timeout, if it exists
 				// Perform tests after a while
 				t = setTimeout(function() {
-					funcs.map(function(func) { // Iterate through validation functions
-						// Test field value
-						var test = func.call(this);
-						// If failed
-						if(test !== true) { 
-							this.errors.push(test); // Add error message
+					if(funcs.length > 0) { // Check if there are any functions to test against
+						var promise = testFuncFactory(funcs[0], this.errors); // Create a promise
+						for (var i = 1; i < funcs.length; i++) {
+							promise = promise.then(testFuncFactory(funcs[i], this.errors)); // Add function to promise chain
 						}
-					}.bind(this));
+					}
 				}.bind(this), dt);
 			}.bind(this), { deep: true });
 		},
 		/**
 		 * Used to test props.field.value against props.test.expression
 		 */
-		isValidAccordingToRegexp: function() {
+		isValidAccordingToRegexp: function(callback) {
 			var test = this.test;
 			var field = this.field;
 			if(test && test.expression && typeof test.expression === 'string') {
 				var valid = RegExp(test.expression).test(field.value);
-				return valid || test.errorMessage || 'Error';
+				callback(valid || test.errorMessage || 'Error');
 			}
 			else {
-				return true;
+				callback(true);
 			}
 		},
 	}
