7b25bfac56ef37005d3fa05ff7343a5b0d61a9bb
Jimmie <jimmie.berg@kb.se>
2015-10-15 11:25:16 +0200

    Added grouping to filterFields

--- src/main/resources/client/components/FilterFields/FilterFields.js
+++ src/main/resources/client/components/FilterFields/FilterFields.js
@@ -1,11 +1,14 @@
 'use strict';
 
 // Vendor
+var Vue = require('vue');
 var _each = require('lodash/collection/each');
 var _cloneDeep = require('lodash/lang/cloneDeep');
 // Mixins
 var FractionalMixin = require('mixins/FractionalMixin/FractionalMixin.js');
 var HelpMixin = require('mixins/HelpMixin/HelpMixin.js')
+// Utils
+var SearchFormUtil = require('utils/SearchFormUtil.js');
 // CSS modules
 var styles = require('!!style!css?modules!./FilterFields.css');
 
@@ -20,6 +23,7 @@ var FilterFields = {
 	template: require('./FilterFields.html'),
 	data: function() {
 		return {
+			filterFieldGroups: {},
 			_styles: styles,
 		}
 	},
@@ -48,6 +52,9 @@ var FilterFields = {
 		}
 	},
 	ready: function() {
+		SearchFormUtil.getFilterFieldGroups(function(filterFieldGroups) {
+			this.filterFieldGroups = filterFieldGroups;
+		}.bind(this));
 		this.initHelp({
 			title: 'FRAKTIONERAD DATA',
 			content: require('./filterFields.Help.html'),
@@ -58,5 +65,30 @@ var FilterFields = {
 		});
 	}
 };
+
+/**
+ * Groups filterFields against filterFieldGroups
+ */
+Vue.filter('filterFieldGroups', function(filterFields, filterFieldGroups) {
+	var groups = {};
+	for(var i = 0; i < filterFields.length; i++) {
+		var field = filterFields[i].field || '';
+		var group = filterFieldGroups[field] || 'none';
+		if(!groups[group]) {
+			groups[group] = [filterFields[i]];
+		}
+		else {
+			groups[group].push(filterFields[i]);
+		}
+	}
+	filterFields = Object.keys(groups).map(function(key, value) {
+		return {
+			$key: key,
+			$value: groups[key],
+		}
+	});
+	return filterFields;
+});
+
 
 module.exports = FilterFields;
