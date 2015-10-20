d479b416e19cc8af3c95c039ee4b09e38c075cd7
Jimmie <jimmie.berg@kb.se>
2015-10-20 08:48:04 +0200

    Hide-button may be disabled by option

--- src/main/resources/client/mixins/FormFieldLayoutMixin/FormFieldLayoutMixin.js
+++ src/main/resources/client/mixins/FormFieldLayoutMixin/FormFieldLayoutMixin.js
@@ -4,6 +4,7 @@
  * This mixin makes a wrapper component available. The wrapper may be used to insert form-field elements
  */
 var FormFieldLayoutMixin = {
+	props: ['hideable'],
 	components: {
 		'form-field-wrapper': {
 			inherit: true,
