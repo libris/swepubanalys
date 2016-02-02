e44594bc177c115994351c1c52186609d85e20b7
Theodor Tolstoy <theodor@tolstoy.se>
2016-02-02 16:34:01 +0100

    CI-test

---
+++ src/main/groovy/Clients/GitHub.groovy
@@ -0,0 +1,13 @@
+package Clients
+
+import wslite.rest.RESTClient
+
+/**
+ * Created by Theodor on 2016-02-02.
+ */
+class GitHub {
+    static Map getReleases() {
+            def restClient = new RESTClient()
+    }
+
+}
